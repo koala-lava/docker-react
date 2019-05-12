a79431ca86d95fb6e3fe8762cfe1c494ae0958aa
Saiyn Dembirel <shgr11111@yandex.ru>
2019-05-12 18:21:10 +0700

    first commit by me

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Wanna hang with me?
         </p>
         <a
           className="App-link"

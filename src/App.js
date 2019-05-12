d1d82990fcaaf35e583c363a5a28042c5e9fbe74
Saiyn Dembirel <shgr11111@yandex.ru>
2019-05-12 22:16:07 +0700

    Changed index text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Wanna hang with me?
+          Just a small change
         </p>
         <a
           className="App-link"

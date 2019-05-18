ea4c20abecace90033c34c8e22694f9496ecb87d
Saiyn Dembirel <shgr11111@yandex.ru>
2019-05-18 14:07:00 +0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Just a small change
+          Changed in feature branch!
         </p>
         <a
           className="App-link"
